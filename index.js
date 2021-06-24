17f302c70e1e6a4820dd27d5cafd565f9587165b
Minku Singh <mailminkusingh@gmail.com>
2021-06-24 21:27:52 +0530

    add js file

---
+++ index.js
@@ -0,0 +1,13 @@
+const colors = ["#34ea20", "red", "purple", "#345672", "orange", "yellow", "#051", "#349", "#230", "#234422"]
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+//after the button is clicked
+btn.addEventListener("click", function()
+    {
+        const randomNumber = Math.floor(Math.random()*colors.length);
+        document.body.style.backgroundColor = colors[randomNumber];
+        color.textContent = colors[randomNumber];
+    }
+)
